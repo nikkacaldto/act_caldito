1af241885c3d7d39a45ca55c8ed50a6aa2b9118a
Nikka <22-37907@g.batstate-u.edu.ph>
2025-05-09 20:46:27 +0800

    Fix: Deploy dist folder instead of build

--- sample-temp/vite.config.js
+++ sample-temp/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/act_caldito/',  // 👈 required for GitHub Pages subpath
+  base: '/act_caldito/',
   plugins: [react()]
 })
